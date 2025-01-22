b713a958432ce6887567cc68b96b176d9487ebd6
Mohamed Atef <mohamed.atef@example.com>
2025-01-22 13:52:32 +0200

    Remove bin/Assignments folder from Git

--- bin/Assignments/assignment 1/vowel or constant.dart
+++
@@ -1,9 +0,0 @@
-import 'dart:io';
-
-void main () {
-  print("enter a letter");
-  String letter = stdin.readLineSync();
-  if (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u' )
-    {print("The alphabet is a vowel");}
-  else {print("The alphabet is a constant");}
-}
--- bin/sessions/OOP/session 1 oop/functions.dart
+++
@@ -1,7 +0,0 @@
-void main (){
-
-  add (3 ,4,);
-}
-int add (int x,int y ){
-  return x+y;
-}
